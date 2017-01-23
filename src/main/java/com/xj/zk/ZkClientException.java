6eed0ce6f2da40504d7caa94f707c86badfa5895
xiajun <xiajunsongfan@users.noreply.github.com>
2017-01-23 11:37:47 +0800

    Update ZkClientException.java
    格式化

--- src/main/java/com/xj/zk/ZkClientException.java
+++ src/main/java/com/xj/zk/ZkClientException.java
@@ -1,1 +1,19 @@
-package com.xj.zk;/** * Author: xiajun * Date: 14/5/20 */public class ZkClientException extends RuntimeException {    public ZkClientException(String msg) {        super(msg);    }    public ZkClientException(String message, Exception e) {        super(message, e);    }    public ZkClientException(Exception e) {        super(e);    }}
+package com.xj.zk;
+
+/**
+ * Author: xiajun
+ * Date: 14/5/20
+ */
+public class ZkClientException extends RuntimeException {
+    public ZkClientException(String msg) {
+        super(msg);
+    }
+
+    public ZkClientException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public ZkClientException(Exception e) {
+        super(e);
+    }
+}
